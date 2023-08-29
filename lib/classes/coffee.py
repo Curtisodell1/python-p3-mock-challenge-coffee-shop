99d80f327c1231f57d54a362397d725866c0d082
Curtis O'Dell <curtisodell1@gmail.com>
2023-08-28 23:23:01 -0600

    working coffee mock challenge

--- lib/classes/coffee.py
+++ lib/classes/coffee.py
@@ -1,17 +1,28 @@
 class Coffee:
     def __init__(self, name):
         self.name = name
-        
-    def orders(self, new_order=None):
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        if isinstance(name, str) and not hasattr(self, "name"):
+            self._name = name
+        else:
+            raise Exception
+
+    def orders(self):
         from classes.order import Order
-        pass
-    
-    def customers(self, new_customer=None):
+        return [order for order in Order.all if order.coffee == self]
+
+    def customers(self):
         from classes.customer import Customer
-        pass
-    
+        return [*set([order.customer for order in self.orders()])]
+
     def num_orders(self):
-        pass
-    
+        return len(self.orders())
+
     def average_price(self):
-        pass
+        return sum([order.price for order in self.orders()])/ self.num_orders()
--- lib/classes/customer.py
+++ lib/classes/customer.py
@@ -1,11 +1,30 @@
+from .order import Order
 class Customer:
+    all = []
     def __init__(self, name):
         self.name = name
-        
-    def orders(self, new_order=None):
-        from classes.order import Order
-        pass
+        Customer.all.append(self)
+
+    @property
+    def name(self):
+        return self._name
     
-    def coffees(self, new_coffee=None):
+    @name.setter
+    def name(self, name):
+        if isinstance(name, str) and 0 < len(name) < 16:
+            self._name = name
+        else:
+            raise Exception
+
+    def orders(self):
+        from classes.order import Order
+        return [order for order in Order.all if order.customer == self]
+    
+    def coffees(self):
         from classes.coffee import Coffee
-        pass
+        return [*set([order.coffee for order in self.orders()])]
+    
+
+    def create_order(self, coffee, price):
+        return Order(self,coffee,price)
+    
